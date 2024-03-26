c76d421b36fd8830aaf74890f9cb7e94b3eb7375
umkament <97944243+umkament@users.noreply.github.com>
2024-03-26 17:46:43 +0300

    fix change name of blog

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -22,7 +22,7 @@ export const Header = () => {
       <Container maxWidth="lg">
         <div className={styles.inner}>
           <Link className={styles.logo} to="/">
-            <div>ARCHAKOV BLOG</div>
+            <div>UMKAMENT</div>
           </Link>
           <div className={styles.buttons}>
             {isAuth ? (
