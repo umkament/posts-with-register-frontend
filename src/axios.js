71d14efe4063fb814820e7c18ed6d3fdd6aade68
umkament <97944243+umkament@users.noreply.github.com>
2024-04-02 23:21:50 +0300

    empty commit

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,7 @@ export const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 })
 
+
 instance.interceptors.request.use((config)=>{
   config.headers.Authorization = window.localStorage.getItem('token')
   return config
