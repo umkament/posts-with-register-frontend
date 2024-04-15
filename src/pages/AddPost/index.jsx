d53f9dbdbbdfeaff10ec4d9d48d8d45edee4672a
umkament <97944243+umkament@users.noreply.github.com>
2024-04-15 12:46:15 +0300

    fix / in <img/>

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -142,7 +142,7 @@ export const AddPost = () => {
           <Button variant="contained" color="error" onClick={onClickRemoveImage}>
             Удалить
           </Button>
-          <img className={styles.image} src={`${process.env.REACT_APP_BLOG_API}${imageUrl}`} alt="Uploaded" />
+          <img className={styles.image} src={`${process.env.REACT_APP_BLOG_API}/${imageUrl}`} alt="Uploaded" />
         </>
       )}
       <br />
