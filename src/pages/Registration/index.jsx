6b45ea4737e57fea0cc2bd7b382e2e3bcb37100c
umkament <97944243+umkament@users.noreply.github.com>
2024-04-15 13:22:24 +0300

    fix navigate after registration

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -36,6 +36,10 @@ export const Registration = () => {
     }
   }
 
+  if (isAuth) {
+    return <Navigate to='/'/>
+  }
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
