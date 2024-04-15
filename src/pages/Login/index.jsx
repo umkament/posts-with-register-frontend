0f6b0ec811f33c478c488ac7d2131c5e70ee3cfd
umkament <97944243+umkament@users.noreply.github.com>
2024-04-15 18:07:40 +0300

    fix CircularProgress, add color

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -33,13 +33,6 @@ export const Login = () => {
   })
 
   const onSubmit = async (values)=>{
-    // const data = await dispatch(fetchAuth(values))
-    // if (!data.payload) {
-    //   return alert('Не удалось авторизоваться!')
-    // }
-    // if ('token' in data.payload) {
-    //   window.localStorage.setItem('token', data.payload.token)
-    // }
     setIsLoading(true); // Устанавливаем isLoading в true перед отправкой запроса
     try {
       const data = await dispatch(fetchAuth(values));
@@ -62,7 +55,7 @@ export const Login = () => {
   console.log('isLoading:', isLoading);
   return (
     <Paper classes={{ root: styles.root }} elevation={4}>
-      {isLoading && <CircularProgress />}
+      {isLoading && <CircularProgress color="success"/>}
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
