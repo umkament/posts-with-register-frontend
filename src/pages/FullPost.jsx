50eb1c54507d704f66eeccbf54778ba08b98fd77
umkament <97944243+umkament@users.noreply.github.com>
2024-04-17 16:13:30 +0300

    add some changes

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -51,14 +51,14 @@ const [isLoading, setIsLoading] = useState(true)
               fullName: "Вася Пупкин",
               avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
             },
-            text: "Это тестовый комментарий 555555",
+            text: "Приложение показывает количество просмотров статьи",
           },
           {
             user: {
               fullName: "Иван Иванов",
               avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
             },
-            text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
+            text: "Ты можешь редактировать и удалять только свои статьи",
           },
         ]}
         isLoading={false}
